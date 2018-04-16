5060f9bd86fa8a376beecea97d04d7380afc2b97
Tfff1OFFICIAL <tfff1OFFICIAL@users.noreply.github.com>
2018-04-16 13:22:32 +1200

    set up setup.py and created readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# mc_launcher_core
+
+A low-level python Minecraft launcher module. This is designed to be integrated as the base of any kind of Minecraft launcher solution, such as the [Simple Mod Installer v3](https://minecraft-mod-installer.weebly.com/).
--- setup.py
+++ setup.py
@@ -1,1 +1,20 @@
-# TODO: this
+from setuptools import setup, find_packages
+
+setup(
+    name="mc_launcher_core",
+    version="0.0.9",
+    license="MIT",
+    url="https://github.com/tfff1OFFICIAL/mc_launcher_core",
+    install_requires=["unpack200>=1.0.1"],
+    python_requires=">=3",
+    project_urls={
+        "Source": "https://github.com/tfff1OFFICIAL/mc_launcher_core"
+    },
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3'
+    ],
+    keywords="minecraft mod launcher",
+    packages=find_packages(exclude=[".idea", "*.ignore*"]),
+    py_modules=["mc_launcher_core"]
+)
